29cc20858b9dce7e7472a9657587726000670c25
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-01-22 12:46:01 -0700

    working

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage (url,left,bottom){
+  let object =  document.createElement ('img')
+  object.src = url
+  object.style.left = left + 'px'
+  object.style.bottom = bottom + 'px'
+  object.style.position = 'fixed'
+  document.body.append(object)
+  return object
+}
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+
+
+
+
+
+newItem('assets/sheild.png',165,185)
+newItem('assets/sword.png',500,405)
+newImage('assets/well.png',500,425)
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png',150,200)
+newItem('assets/staff.png',600,100)
